47aa2e668b7d526919392a7aa0428a95b6fb6af9
sima-coder <sihem.bellez@yahoo.fr>
2020-06-06 18:22:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import imageInSrc from './imageInSrc.jpg';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ border:"solid 1px black", maxWidth:"100vw"}}>  
+        <h1 className="title red">Sihem BELLEZ</h1>  
+        <br/>  
+        <img src={imageInSrc} /> 
+        <br/> 
+        <img src="/imageInPublic.jpg" />
+
+      </div>  
+
+      <video width={320} height={240} controls>  
+        <source src="myVideo.mp4" type="video/mp4"></source> 
+      </video> 
+    </>
+
   );
 }
 
